1fa9cd077934eeb80970d12fdff6f2d34c6e28ec
cesareal9 <caugustorama@gmail.com>
2024-09-04 20:37:14 -0300

    trabalho finalizado

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const estudante_router = require("./routes/estudante.js")
+const livro_router = require("./routes/livro.js")
+const aluguel_router = require("./routes/aluguel.js")
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+
+app.use("/estudante", estudante_router)
+app.use("/alguel", aluguel_router)
+app.use("/rede", livro_router)
+
+app.listen(port, () => {
+    console.log(`Servidor iniciado na porta! ${port} port`)
+})
